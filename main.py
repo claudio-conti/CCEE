a9a45c979ea543c1df5b479a180252a8e7afe692
claudio-conti <claudio.conti.ribeiro02@gmail.com>
2023-06-15 16:16:33 -0300

    Second commit

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ def update_hash(*args):
     hash_txt = ''
 
     # Cria um loop para concatenar todos os argumentos
-    # passados pela função.
-    for arg in args:
-        hash_txt += str(arg)
+    # passados pela função em um String (hash_text).
+    # Este loop for in passa por todos os elementos dentro do array
+    for a in args:
+        hash_txt += str(a)
 
+    # O update() é para adicionar uma variável no hash antes de
+    # criptografar, é necessário no mínimo um update para funcionar
+    # a criptografia.
+    # O encode() é necessário para transformar o hash_txt em binário,
+    # E assim conseguir criptografar ele.
     h.update(hash_txt.encode())
-    print(h.hexdigest())
+
+    # O hexdigest() pega todas as variáveis lançadas no update(),
+    # e criptografa todas elas em um hash contendo só hexadecimais.
     return h.hexdigest()
 
 
 # Isso roda o script de python.
 if __name__ == '__main__':
-    update_hash('batataassada')
+    update_hash('batataassada', 1454)
